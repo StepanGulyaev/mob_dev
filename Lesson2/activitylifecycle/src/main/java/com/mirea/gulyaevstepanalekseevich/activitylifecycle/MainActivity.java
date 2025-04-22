ac502a5e968dd0ad9f66af845197e9a51c2d4fb7
Stepan Gulyaev <st.gulyaev@yadro.com>
2025-04-22 23:47:12 +0300

    activityLifecycle done

---
+++ Lesson2/activitylifecycle/src/main/java/com/mirea/gulyaevstepanalekseevich/activitylifecycle/MainActivity.java
@@ -0,0 +1,87 @@
+package com.mirea.gulyaevstepanalekseevich.activitylifecycle;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+}
+
+// 1. Будет ли вызван метод «onCreate» после нажатия на кнопку «Home» и возврата в приложение?
+// Ответ: Нет, вызывается OnResume
+
+// 2. Изменится ли значение поля «EditText» после нажатия на кнопку «Home» и возврата в приложение?
+// Ответ: Нет, текст останется таким же, каким и был
+
+// 3. Изменится ли значение поля «EditText» после нажатия на кнопку «Back» и возврата в приложение?
+// Ответ: Да, поле для набора текста очистится, так как в данном случае при нажатии на back срабатывает onDestroy. А при новом заходе срабатывает onCreate
+
+
