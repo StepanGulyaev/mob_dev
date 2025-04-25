da99ffbb7fad025c50b2d3d19c0d536aa008fadf
Stepan Gulyaev <st.gulyaev@yadro.com>
2025-04-25 23:35:22 +0300

    NotificationApp done

---
+++ Lesson2/notificationapp/src/main/java/com/mirea/gulyaevstepanalekseevich/notificationapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.mirea.gulyaevstepanalekseevich.notificationapp;
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int PermissionCode = 200;
+    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+            Log.d(MainActivity.class.getSimpleName().toString(),"Permissions granted");
+        }
+        else {
+            Log.d(MainActivity.class.getSimpleName().toString(),"Permissions denied");
+            ActivityCompat.requestPermissions(this,new String[]{POST_NOTIFICATIONS},PermissionCode);
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClickSendNotification (View view) {
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Congratulation!")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Gulyaev SA needs you!"))
+                .setContentTitle("Mirea");
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Gulyaev SA Notification", importance);
+                channel.setDescription("MIREA Channel");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.
+                from(this);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(1, builder.build());
+    }
+}
